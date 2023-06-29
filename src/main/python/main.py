9aeb252245f81c8f0733ecd82548b3010022ce11
saulo.bergamo <saulo.bergamo@dextra-sw.com>
2023-06-29 03:33:37 -0300

    adjust cngr() and made new methods to load_model_matrix(), reshape and get matrix name

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,4 +1,5 @@
 import base64
+import random
 import sys
 import numpy as np
 import cv2 as cv
@@ -12,14 +13,17 @@ import os
 PATH = os.path.expanduser("~/utfpr/Desenvolvimento Integrado de Sistemas/images-rebuild-by-sign/src/main/resources/")
 
 
-def get_sign_from_DB(key):
+def get_sign_from_DB(image_id):
     client = pymongo.MongoClient("mongodb://admin:admin@localhost:27017/")
     db = client['BSI-DIS']
     collection = db['entry_sign_to_rebuild_image']
 
     # find one document
-    entry = collection.find_one({"imageId": key})
-    return entry['entrySignDouble']
+    entry = collection.find_one({"imageId": image_id})
+    response = entry['entrySignDouble']
+    response = np.array(response, dtype=np.float64)
+    response.shape = (response.shape[0], 1)
+    return response
 
 
 def save_image(data):
@@ -57,114 +61,107 @@ def signal_gain(sign_type):
             gamma = 100 + (1 / 20) * l * (l ** 0.5)
             gl, c = gl, c * gamma
 
+def load_model_matrix(matrix_name):
+    matrix = np.load(PATH + f'pickle/{matrix_name}.pickle', allow_pickle=True)
+    matrix = np.asarray(matrix, dtype=np.float64)
+    return matrix
 
-def cgne(key, sign_type, user_name):
-    start_time = time.time()
-
-    # r0=g−Hf0
-    r = get_sign_from_DB(key)
-    r = np.array(r, dtype=np.float64)
-    r.shape = (r.shape[0], 1)
-
-    if r.shape == (50816, 1):
+def get_matrix_model_name(sign_type):
+    if sign_type == 'true':
         matriz_name = 'H-1'
     else:
         matriz_name = 'H-2'
+    return matriz_name 
 
-    matriz = np.load(PATH + f'pickle/{matriz_name}.pickle', allow_pickle=True)
-    matriz = np.asarray(matriz, dtype=np.float64)
+def image_reshape(image, sign_type):
+    image = image - image.min()
+    image = image / image.max()
+    image = image * 255
+    if sign_type == 'true':
+        image = image.reshape((60, 60), order='F')
+    else:
+        image = image.reshape((30, 30), order='F')
+    return image
+
+def cgne(image_id, sign_type, user_name):
+    start_time = time.time()
+
+    # r0=g−Hf0
+    entry_sign = get_sign_from_DB(image_id)
+    matrix = load_model_matrix(get_matrix_model_name(sign_type))
 
     # p0=HTr0
-    p = np.matmul(matriz.T, r)
+    p = np.matmul(matrix.T, entry_sign)
 
     # f0=0
     image = np.zeros_like(len(p))
 
     count = 1
-    erro = 0
-    while erro < float(1e10 - 4):
+    error = 0
+    while error < float(1e10**(-4)):
         # αi=rTiripTipi
-        alpha = np.dot(r.T, r) / np.dot(p.T, p)
+        alpha = np.dot(entry_sign.T, entry_sign) / np.dot(p.T, p)
 
         # fi+1=fi+αipi
         image = image + alpha * p.T
 
         # ri+1=ri−αiHpi
-        ri = r - alpha * np.dot(matriz, p)
+        ri = entry_sign - alpha * np.dot(matrix, p)
 
         # ϵ=||ri+1||2−||ri||2
-        erro += np.linalg.norm(r, ord=2) - np.linalg.norm(ri, ord=2)
-        if erro < 1e10 - 4:
+        error += np.linalg.norm(entry_sign, ord=2) - np.linalg.norm(ri, ord=2)
+        if error < 1e10 - 4:
             break
 
         # βi=rTi+1ri+1rTiri
-        beta = np.dot(r.T, ri) / np.dot(ri.T, r)
+        beta = np.dot(entry_sign.T, ri) / np.dot(ri.T, entry_sign)
 
         # pi+1=HTri+1+βipi
-        p = np.dot(matriz.T, ri) + beta * p
+        p = np.dot(matrix.T, ri) + beta * p
 
         count += 1
 
-    image = image - image.min()
-    image = image / image.max()
-    image = image * 255
 
-    if matriz_name == 'H-1':
-        image = image.reshape((60, 60), order='F')
-    else:
-        image = image.reshape((30, 30), order='F')
-
+    image = image_reshape(image, sign_type)
     normalized_image = normalize(image)
-
-    # Mostra a imagem gerada
-    #     plt.imshow(normalized_image, cmap='gray')
-    #     plt.title('Log')
-    #     plt.show()
-
-    # final = cv.resize(image, None, fx=10, fy=10, interpolation=cv.INTER_AREA)
-
-    run_time = time.time() - start_time
     image_array_list = normalized_image.tolist()
     process = psutil.Process()
     memory = process.memory_info().rss / 1000000
-
+    run_time = time.time() - start_time
+    
     data = {
-        "clientId": key,
+        "userName": user_name,
+        "imageId": image_id,
         "iterations": count,
         "runTime": run_time,
-        "error": erro,
+        "error": error,
         "memory": memory,
-        "image": image_array_list,
-        #         "process": process,
+        "signType": sign_type,
+        "image": image_array_list
     }
 
     #     print(count)
     #     print(run_time)
     #     print(str(memory) + " MB")
     #     print(erro)
-    #     plt.imshow(image, cmap='gray')
-    #     plt.title('Log')
-    #     plt.show()
+    # #   Imprime a imagem gerada
+    plt.imshow(normalized_image, cmap='gray')
+    plt.title('Log')
+    plt.show()
+
     # # Salvar imagem localmente
     #     cv.imwrite(PATH+ 'images/teste2.png', final)
 
     # Salvar imagem no banco MONGODB
     save_image(data)
 
-
-def cgnr():
+def cgnr(image_id, sign_type, user_name):
     start_time = time.time()
-
-    matriz = np.load(PATH + 'pickle/H-1.pickle', allow_pickle=True)
-    matriz = np.asarray(matriz, dtype=np.float64)
-
-    # r0=g−Hf0
-    r = np.loadtxt(PATH + 'csv/G-2.csv', delimiter=',',
-                   dtype=np.float64)
-    r.shape = (r.shape[0], 1)
+    entry_sign = get_sign_from_DB(image_id)
+    matrix = load_model_matrix(get_matrix_model_name(sign_type))
 
     # z0=HTr0
-    p = np.matmul(matriz.T, r)
+    p = np.matmul(matrix.T, entry_sign)
     z = p
 
     # f0=0
@@ -172,9 +169,9 @@ def cgnr():
 
     count = 1
     erro = 0
-    while erro < 1e10 - 4:
+    while erro < 1e10**(-4):
         # wi=Hpi
-        w = np.matmul(matriz, p)
+        w = np.matmul(matrix, p)
 
         # αi=||zi||22/||wi||22
         alpha = np.linalg.norm(z, ord=2) ** 2 / np.linalg.norm(w, ord=2) ** 2
@@ -183,10 +180,10 @@ def cgnr():
         image = image + alpha * p.T
 
         # ri+1=ri−αiwi
-        ri = r - alpha * w
+        ri = entry_sign - alpha * w
 
         # zi+1=HTri+1
-        z = np.matmul(matriz.T, ri)
+        z = np.matmul(matrix.T, ri)
 
         # βi=||zi+1||22/||zi||22
         beta = np.linalg.norm(z, ord=2) ** 2 / np.linalg.norm(z, ord=2) ** 2
@@ -195,30 +192,44 @@ def cgnr():
         p = z + beta * p
 
         # ϵ=||ri+1||2−||ri||2
-        erro = np.linalg.norm(ri, ord=2) - np.linalg.norm(r, ord=2)
-        if erro < 1e10 - 4:
+        erro = np.linalg.norm(ri, ord=2) - np.linalg.norm(entry_sign, ord=2)
+        if erro < 1e10**(-4):
             break
 
         count += 1
 
-    image = image - image.min()
-    image = image / image.max()
-    image = image * 255
+    image = image_reshape(image, sign_type)
+    normalized_image = normalize(image)
+    image_array_list = normalized_image.tolist()
+    process = psutil.Process()
+    memory = process.memory_info().rss / 1000000
+    run_time = time.time() - start_time
+    
+    data = {
+        "userName": user_name,
+        "imageId": image_id,
+        "iterations": count,
+        "runTime": run_time,
+        "error": erro,
+        "memory": memory,
+        "signType": sign_type,
+        "image": image_array_list
+    }
 
-    image = image.reshape((60, 60), order='F')
+    save_image(data)
 
     final = cv.resize(image, None, fx=10, fy=10, interpolation=cv.INTER_AREA)
 
-    cv.imwrite(PATH + 'images/testecgnr.png', final)
-    run_time = time.time() - start_time
-    print(count)
-    print(run_time)
+    # cv.imwrite(PATH + 'images/testecgnr.png', final)
 
-    return image, count, run_time
+def main(image_id, sign_type, user_name):
 
-
-def main(key, sign_type, user_name):
-    cgne(key, sign_type, user_name)
+    algorithms = ['cgne', 'cgnr']
+    algorithm = random.choice(algorithms)
+    if(algorithm == 'cgne'):
+        cgne(image_id, sign_type, user_name)
+    else:
+        cgnr(image_id, sign_type, user_name)
 
 
 if __name__ == '__main__':
